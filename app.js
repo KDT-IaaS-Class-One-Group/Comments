f80226b9f8f3d6b4450b1ccbd37f1cd77073f68a
kimwoojin123 <kimwoojin1903@gmail.com>
2023-10-26 18:02:04 +0900

    서버 수정했엉 ^^;;

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const bodyParser = require("body-parser");
 const app = express();
+const port = 3000;
 
 // 이미지 업로드 설정
 const storage = multer.diskStorage({
@@ -18,13 +19,13 @@ const upload = multer({ storage });
 // 미들웨어 설정
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); // 정적 파일 제공을 위한 폴더 설정 (subindex.html 파일이 있어야 함)
 
 // 이미지와 텍스트 데이터를 저장할 배열
 const data = [];
 
 // 이미지 업로드 및 데이터 추가 엔드포인트 설정
-app.post("/submit", upload.single("image"), (req, res) => {
-  // 이미지 업로드 후 data 배열에 이미지 정보와 텍스트 추가
+app.post("/upload", upload.single("image"), (req, res) => {
   const item = {
     imagePath: req.file.path,
     text: req.body.text,
@@ -33,15 +34,11 @@ app.post("/submit", upload.single("image"), (req, res) => {
   res.status(200).send("이미지 업로드 및 데이터 추가 완료");
 });
 
-// 서버에서 데이터 배열을 반환하는 엔드포인트 설정
+// 이미지와 텍스트 데이터를 반환하는 엔드포인트 설정
 app.get("/data", (req, res) => {
   res.json(data);
 });
 
-// 서브페이지 서비스 (public 폴더 내에 subindex.html 파일 있어야 함)
-app.use(express.static("public"));
-
-// 서버 시작
-app.listen(3000, () => {
-  console.log("서버가 3000번 포트에서 실행 중입니다.");
+app.listen(port, () => {
+  console.log(`서버가 ${port}번 포트에서 실행 중입니다.`);
 });
