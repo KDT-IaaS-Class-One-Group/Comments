f5418804f027ee4ffbcb5993a50d965187f872d0
kimwoojin123 <kimwoojin1903@gmail.com>
2023-10-27 12:00:55 +0900

    내가해냄 ^^:

--- server.js
+++ server.js
@@ -22,54 +22,40 @@ app.get("/sub", (req, res) => {
 });
 
 let uploadCounter = 0;
+let uploadCounter2 = 0;
 
 app.post("/upload", (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send("파일이 업로드되지 않았습니다.");
   }
 
-  const uploadedFile = req.files.image;
+  const uploadedImage = req.files.image;
 
-  const newFileName = `image${++uploadCounter}.jpg`;
-
-  const uploadPath = path.join(__dirname, "uploads", newFileName);
-
-  uploadedFile.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).send("파일 업로드에 실패했습니다.");
-    }
-
-    res.redirect("/sub");
-  });
-});
-
-app.get("/get-image", (req, res) => {
-  const filesInUploads = fs.readdirSync(path.join(__dirname, "uploads"));
-  res.json(filesInUploads); // 이미지 파일 이름 목록을 JSON 형식으로 전달
-});
-
-const chatLogFile = "chatlog.txt";
-
-// 추가된 부분: 텍스트를 txt 파일로 저장
-io.on("connection", (socket) => {
-  // 클라이언트로부터 메시지를 받아 메모장에 저장
-  socket.on("message", (message) => {
-    fs.appendFile(chatLogFile, message + "\n", (err) => {
+  // 이미지 파일 업로드 처리
+  if (uploadedImage) {
+    const newImageName = `image${++uploadCounter}.jpg`;
+    const imagePath = path.join(__dirname, "uploads", newImageName);
+    uploadedImage.mv(imagePath, (err) => {
       if (err) {
-        console.error("메시지를 저장하는 중 오류 발생");
+        return res.status(500).send("이미지 파일 업로드에 실패했습니다.");
       }
     });
+  }
 
-    // 모든 클라이언트에 메시지 브로드캐스트
-    io.emit("message", message);
-  });
+  if (req.body.text) {
+    const text = req.body.text;
+    const newTextName = `text${++uploadCounter2}.txt`;
+    const textPath = path.join(__dirname, "uploads", newTextName);
 
-  // 클라이언트와의 연결이 끊겼을 때
-  socket.on("disconnect", () => {
-    console.log("클라이언트와의 연결이 끊겼습니다.");
-  });
+    fs.writeFile(textPath, text, (err) => {
+      if (err) {
+        return res.status(500).send("텍스트 파일 업로드에 실패했습니다.");
+      }
+    });
+  }
+
+  res.redirect("/sub");
 });
-
 app.listen(port, () => {
   console.log(`서버가 ${port} 포트에서 실행 중입니다.`);
 });
